21e1254c77c625a04325ce5e583d0d7cb6906cf2
Alex Florenti <xela92@MBP-di-Axel.lan>
2016-01-20 19:18:40 +0100

    cleanup

--- src/jmupen/JMupenUpdater.java
+++ src/jmupen/JMupenUpdater.java
@@ -10,15 +10,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
 import org.apache.commons.io.FileUtils;
 
 /**
